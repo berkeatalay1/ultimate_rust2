7bd9ce63fd69dacad96a266cd7580fbe95152b6f
Berke <tormorkor@outlook.com>
2023-03-18 14:52:34 +0300

    feat: docs

--- exercise/docs/src/lib.rs
+++ exercise/docs/src/lib.rs
@@ -1,7 +1,6 @@
 // 1. Thank you for volunteering to document our pumpkin library! Let's start by grabbing the first
 // paragraph from https://en.wikipedia.org/wiki/Pumpkin and pasting it as our module-level
 // documentation. Hint: Use inner-documentation comments.
-//
 // Once you've got the documentation here, run `cargo doc --no-deps --open` and take a look!
 
 // 2. What about an image!? Add an image of a pumpkin to the end of the module-level documentation.
@@ -15,8 +14,19 @@
 // - Document the "roundness" field, explaining that it is a percentage
 // - Document the "orangeness" field, explaining that it is a number from 8 to 27
 
+//! A pumpkin is a vernacular term for mature winter squash of species and varieties in the genus Cucurbita that has culinary and cultural significance but no agreed upon botanical or scientific meaning. 
+//! The term pumpkin is sometimes used interchangeably with "squash" or "winter squash", and is commonly used for cultivars of Cucurbita argyrosperma, Cucurbita ficifolia, Cucurbita maxima, Cucurbita moschata, and Cucurbita pepo.
+//! ![Pumpkin](https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/FrenchMarketPumpkinsB.jpg/700px-FrenchMarketPumpkinsB.jpg) 
+
+/// Big orange thing
+/// 
+/// # Recipes
+/// 
+/// Recipes will be coming soon
 pub struct Pumpkin {
+    /// It is a percentage
     pub roundness: f32,
+    /// It is a number from 8 to 27
     pub orangeness: i32,
 }
 
@@ -24,12 +34,15 @@ pub struct Pumpkin {
 // can't be used for pie. :'-(
 
 impl Pumpkin {
+    /// Smash method destroys itself so it will be gone. 
+    /// If you smash  you cant use it for pie
     pub fn smash(self) {}
 }
 
 // 5. Document that BURNT_ORANGE is for the "orangeness" field in the Pumpkin struct.
 // - Link to the Pumpkin struct in your description
 
+/// This is orangeness for [`Pumpkin`]
 pub const BURNT_ORANGE: i32 = 13;
 
 // Challenge: Find the option to pass to `cargo doc` so that documentation for this private item
